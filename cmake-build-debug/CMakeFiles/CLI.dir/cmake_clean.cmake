576f3ca5ad538ea7e689b9bc97f4ba426c6a0844
Joe Samyn <joseph.j.samyn@gmail.com>
2020-04-04 20:29:56 -0700

    Added CLion project folders

---
+++ cmake-build-debug/CMakeFiles/CLI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLI"
+  "CLI.pdb"
+  "CMakeFiles/CLI.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
